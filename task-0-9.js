c33cd156555a141917e2a2ec6f61492f955a6c41
Mary Mbirua <Mbirua>
2021-10-21 17:28:46 +0300

    level-0-coding-challenges q git commit -m level-0-coding-challenges $ git commit -m level-0-coding-challenges

--- task-0-9.js
+++ task-0-9.js
@@ -1,6 +1,22 @@
-function printOutVowels (letters) {
-  var vowels = (a,e,i,o,u)
- return  vowels;
- }
-console.log(printOutVowels("google"));
+function printOutVowels (mainStr) {
+  let myStr = mainStr.toLowerCase();
+  let vowelsPresent = "";
+  if (myStr.includes ("a"))  {
+ vowelsPresent  = vowelsPresent + "a";
+}
+ if (myStr.includes("e")) {
+vowelsPresent = vowelsPresent + "e";
+}
+if (myStr.includes("i")) {
+ vowelsPresent = vowelsPresent + "i";
+}
+ if (myStr.includes("o")) {
+ vowelsPresent = vowelsPresent + "o";
+}
+if (myStr.includes("u")) {
+ vowelsPresent = vowelsPresent + "u";
+}
+ return vowelsPresent;
+}
+ console.log(printOutVowels("Attention"));
 
