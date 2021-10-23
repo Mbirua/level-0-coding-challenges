7e3961c7100556896b1eaf1c2eed372093769cf3
Mary Mbirua <Mbirua>
2021-10-23 14:58:30 +0300

    level-0-coding-challenges

--- task-0-6.js
+++ task-0-6.js
@@ -1,2 +1,12 @@
- console.log(Math.max(56,197,77));
-
+function maxNumber(num1,num2,num3) {
+  var max;
+if (num1 > num2 && num1 > num3){
+ max = num1;
+}
+else if (num2  > num1 && num2 > num3) {
+ max = num2;
+  }
+ else { max = num3;
+ return max; }
+}
+ console.log(maxNumber(45,67,97));
