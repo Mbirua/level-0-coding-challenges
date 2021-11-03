1897e93d1a5b3b630d02ee08bf70e8521040b45a
Mbirua <91622756+Mbirua@users.noreply.github.com>
2021-11-03 16:25:55 +0300

    Update task-0-6.js

--- task-0-6.js
+++ task-0-6.js
@@ -1,9 +1,9 @@
 function maxNumber(num1,num2,num3) {
   var max;
-if (num1 > num2 && num1 > num3){
+if (num1 >= num2 && num1 >= num3){
  max = num1;
 }
-else if (num2  > num1 && num2 > num3) {
+else if (num2  >= num1 && num2 >= num3) {
  max = num2;
   }
  else { max = num3;
